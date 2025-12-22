ba63b1fa5a29de728ac5cfbdec6cfb66cc0e7d2e
Adrian Palmer <adrian.palmer@vista.com>
2025-12-22 11:24:49 +1100

    Add extra metadata

--- src/Markdown/ResolvedMetadata.php
+++ src/Markdown/ResolvedMetadata.php
@@ -4,10 +4,14 @@ namespace Limb\Markdown;
 
 final class ResolvedMetadata
 {
+    /**
+     * @param array<string, mixed> $extra
+     */
     public function __construct(
         public readonly string $title,
         public readonly string $slug,
         public readonly \DateTime $createdAt,
         public readonly ?\DateTime $updatedAt,
+        public readonly array $extra,
     ) {}
 }
